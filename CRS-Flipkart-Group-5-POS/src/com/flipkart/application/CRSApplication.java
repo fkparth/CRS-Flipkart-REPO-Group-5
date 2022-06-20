d5f854e81b38075c36f989bbe7a43c3772a6035c
Peravala Rohitdatta <rohitdatta.p@BLRETV-PQ3H4YY3.local>
2022-06-20 21:30:14 +0530

    added basic menu without functionality

--- CRS-Flipkart-Group-5-POS/src/com/flipkart/application/CRSApplication.java
+++ CRS-Flipkart-Group-5-POS/src/com/flipkart/application/CRSApplication.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class CRSApplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to CRS Application");
-        System.out.println("1.Login");
-        System.out.println("2.Registration Of the Student");
-        System.out.println("3.Update Password");
-        System.out.println("4.Exit");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("\t \t Welcome to CRS Application");
+        System.out.println("Enter 1 Login");
+        System.out.println("Enter 2 for Registration Of the Student");
+        System.out.println("Enter 3 to Update Password");
+        System.out.println("Enter 4 to Exit");
+        System.out.println("Enter Here:");
         int menuClick = sc.nextInt();
         if(menuClick==1){
             System.out.println("Please enter userName");
@@ -18,18 +20,18 @@ public class CRSApplication {
 
             int roleId = sc.nextInt();
             if(roleId==1){
-                StudentCRSMenu temp = new StudentCRSMenu();
-                temp.showChoices();
+                StudentCRSMenu studentMenu = new StudentCRSMenu();
+                studentMenu.showChoices();
 
             }
             else if(roleId==2){
-                ProfessorCRSMenu temp = new ProfessorCRSMenu();
-                temp.showChoices();
+                ProfessorCRSMenu profMenu = new ProfessorCRSMenu();
+                profMenu.showChoices();
 
             }
             else{
-                AdminCRSMenu temp = new AdminCRSMenu();
-                temp.showChoices();
+                AdminCRSMenu adminMenu = new AdminCRSMenu();
+                adminMenu.showChoices();
 
             }
 
